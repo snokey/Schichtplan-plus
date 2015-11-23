65f280db8050eba3a687e2b9defd5f351bbac95f
snokey <snokeyx@gmail.com>
2015-11-23 18:25:07 +0100

    no message

--- Dokumentation/stand01_prog.php
+++ Dokumentation/stand01_prog.php
@@ -160,5 +160,32 @@ private $dayLabels = array("Montag","Dienstag","Mittwoch","Donnerstag","Freitag"
 
 
 
+private function _weeksInMonth($month=null,$year=null){
+         
+        if( null==($year) ) {
+            $year =  date("Y",time()); 
+        }
+         
+        if(null==($month)) {
+            $month = date("m",time());
+        }
+         
+        
+        $daysInMonths = $this->_daysInMonth($month,$year);
+         
+        $numOfweeks = ($daysInMonths%7==0?0:1) + intval($daysInMonths/7);
+         
+        $monthEndingDay= date('N',strtotime($year.'-'.$month.'-'.$daysInMonths));
+         
+        $monthStartDay = date('N',strtotime($year.'-'.$month.'-01'));
+         
+        if($monthEndingDay<$monthStartDay){
+             
+            $numOfweeks++;
+         
+        }
+         
+        return $numOfweeks;
+    }
 
 
