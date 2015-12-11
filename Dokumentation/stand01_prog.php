fdc9190601467b4349711b7983dbb2cffe558303
Markus Alpers <markus.alpers@haw-hamburg.de>
2015-12-11 12:46:30 +0100

    Feedback Alpers

--- Dokumentation/stand01_prog.php
+++ Dokumentation/stand01_prog.php
@@ -36,7 +36,14 @@ private $dayLabels = array("Montag","Dienstag","Mittwoch","Donnerstag","Freitag"
         $year  = null;
          
         $month = null;
-		
+		/*
+		* === Feedback Alpers, Dez 11 ===
+		*
+		* Arbeiten Sie bitte mit Einrückungen, damit erkennbar ist, welche Teile in welchem Bereich eingeordnet sind.	
+		* Hier wirkt es beispielsweise, als wenn die Kontrollstruktur eine eigene Funktion wäre und nicht zu show() gehören würde.
+		*
+		* === Feedback Alpers, Ende ===
+		*/
 	if(null==$year&&isset($_GET['year'])){
 		$year = $_GET['year'];
 	}else if(null==$year){
@@ -61,6 +68,15 @@ private $dayLabels = array("Montag","Dienstag","Mittwoch","Donnerstag","Freitag"
          
         $this->daysInMonth=$this->_daysInMonth($month,$year);
 		
+		/*
+		* === Feedback Alpers, Dez 11 ===
+		*
+		* Lassen Sie bitte diesen Teil (und andere, in denen HTML enthalten ist)
+		* von der HTML'erin der Gruppe überarbeiten: Es soll HTML 5 verwendet werden,
+		* das hier ist leider nur 4.01.
+		*
+		* === Feedback Alpers, Ende ===
+		*/
 		$content='<div id="calendar">'.
                         '<div class="box">'.
                         $this->_createNavi().
@@ -92,6 +108,17 @@ private $dayLabels = array("Montag","Dienstag","Mittwoch","Donnerstag","Freitag"
 	
 	
 	
+	/*
+	* === Feedback Alpers, Dez 11 ===
+	*
+	* Diese Funktion ist (wie einige andere) ein gutes Beispiel dafür, warum
+	* Kommentare im Quellcode wichtig sind: Es ist nicht auf anhieb erkennbar, was
+	* diese Funktion tut, bzw. warum es sie gibt.
+	*
+	* Bitte ergänzen Sie in solchen Fällen einen kurzen Kommentar.
+	*
+	* === Feedback Alpers, Ende ===
+	*/
 	
 	private function _showDay($cellNumber){
          
@@ -203,3 +230,18 @@ private function _weeksInMonth($month=null,$year=null){
 
 
 ?>
+
+<!--
+=== Feedback Alpers, Dez 11 ===
+
+Auch für Sie der Hinweis: Eine Dokumentation sollte (außer wenn das unumgänglich ist, um effizient auf einen Sachverhalt zu verweisen)
+ausschließlich dazu genutzt werden, um zu beschreiben, auf welchem Stand sich das Projekt gerade befindet.
+
+Für einen nicht am Projekt beteiligten muss durch die Dokumentation schnell erkennbar sein, welche Entwicklung das
+Projekt genommen hat, wo die einzelnen Programmteile stehen und warum welche Entscheidungen getroffen wurden.
+
+Das ist hier leider gar nicht der Fall.
+
+Leider kann ich momentan (das gilt für das gesamte Projekt, nicht nur Ihren Anteil) noch keine Interaktivität für Nutzer erkennen.
+
+=== Feedback Alpers, Ende ===
